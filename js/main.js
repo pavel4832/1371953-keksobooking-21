3e9eb07b75111eb13e8290ede6158b2780146256
Pavel <63406282+pavel4832@users.noreply.github.com>
2020-10-02 02:15:25 +0300

    Доработка

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 const MAP = document.querySelector(`.map`);
 const MAP_PIN_MAIN = MAP.querySelector(`.map__pin--main`);
 const PinsDimensions = {
-  WIDTH: 65,
-  HEIGHT: 87
+  WIDTH: 62,
+  HEIGHT: 84
 };
 const MAP_PINS = document.querySelector(`.map__pins`);
 const PIN_TEMPLATE = document.querySelector(`#pin`)
@@ -34,10 +34,8 @@ const MIN_Y_POSITION = 200;
 const MAX_Y_POSITION = 700;
 const OFFSET_X = 25;
 const OFFSET_Y = 70;
-const FILTERS_FORM = MAP.querySelector(`.map__filters`);
-const FILTERS_FIELDS = FILTERS_FORM.children;
 const NOTICE_FORM = document.querySelector(`.ad-form`);
-const NOTICE_FIELDS = NOTICE_FORM.querySelectorAll(`fieldset`);
+const FORM_FIELDS = document.querySelectorAll(`.map__filters select, .map__filters fieldset, .ad-form fieldset`);
 const ADDRESS_FIELD = NOTICE_FORM.querySelector(`#address`);
 const ROOM_FIELD = NOTICE_FORM.querySelector(`#room_number`);
 const GUEST_FIELD = NOTICE_FORM.querySelector(`#capacity`);
@@ -108,15 +106,15 @@ const renderPins = function (pins) {
   MAP_PINS.appendChild(FRAGMENT);
 };
 
-const disableFormFields = function (target) {
-  for (let i = 0; i < target.length; i++) {
-    target[i].setAttribute(`disabled`, `disabled`);
+const disableFormFields = function (fields) {
+  for (let i = 0; i < fields.length; i++) {
+    fields[i].setAttribute(`disabled`, `disabled`);
   }
 };
 
-const enableFormFields = function (target) {
-  for (let i = 0; i < target.length; i++) {
-    target[i].removeAttribute(`disabled`);
+const enableFormFields = function (fields) {
+  for (let i = 0; i < fields.length; i++) {
+    fields[i].removeAttribute(`disabled`);
   }
 };
 
@@ -240,41 +238,51 @@ const fillAddressField = function () {
   ADDRESS_FIELD.value = `${xLocation}, ${yLocation}`;
 };
 
-const getActivePage = function () {
+const activatePage = function () {
   const PINS = getPins();
 
-  enableFormFields(FILTERS_FIELDS);
-  enableFormFields(NOTICE_FIELDS);
+  enableFormFields(FORM_FIELDS);
   MAP.classList.remove(`map--faded`);
   NOTICE_FORM.classList.remove(`ad-form--disabled`);
-  fillAddressField();
   renderPins(PINS);
   renderCard(PINS[0]);
 };
 
-disableFormFields(FILTERS_FIELDS);
-disableFormFields(NOTICE_FIELDS);
-
-MAP_PIN_MAIN.addEventListener(`mousedown`, function (evt) {
-  if (evt.button === 0) {
-    getActivePage();
-  }
-});
-
-MAP_PIN_MAIN.addEventListener(`keydown`, function (evt) {
-  if (evt.key === `Enter`) {
-    getActivePage();
-  }
-});
-
-GUEST_FIELD.addEventListener(`change`, function () {
+const checkValidGuest = function () {
   let roomNumber = ROOM_FIELD.value;
   let guestNumber = GUEST_FIELD.value;
 
-  if (roomNumber < guestNumber) {
+  if (roomNumber === `100` && guestNumber !== `0`) {
+    GUEST_FIELD.setCustomValidity(`Это жилье не для гостей. Измените выбор`);
+  } else if (roomNumber < guestNumber) {
     GUEST_FIELD.setCustomValidity(`Количество гостей превышает количество комнат. Уменьшите количество гостей`);
   } else {
     GUEST_FIELD.setCustomValidity(``);
   }
   GUEST_FIELD.reportValidity();
+};
+
+fillAddressField();
+disableFormFields(FORM_FIELDS);
+
+MAP_PIN_MAIN.addEventListener(`mousedown`, function (evt) {
+  if (evt.button === 0) {
+    activatePage();
+    checkValidGuest();
+  }
+});
+
+MAP_PIN_MAIN.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    activatePage();
+    checkValidGuest();
+  }
+});
+
+GUEST_FIELD.addEventListener(`change`, function () {
+  checkValidGuest();
+});
+
+ROOM_FIELD.addEventListener(`change`, function () {
+  checkValidGuest();
 });
