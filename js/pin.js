deda748102340d4bc3109129951a1c5e2ddcca18
Pavel Ulianov <paveluv@mail.ru>
2020-10-08 09:20:20 +0300

    Make own project, task 6_1, get net data.

--- js/pin.js
+++ js/pin.js
@@ -1,27 +1,22 @@
 'use strict';
 
 (function () {
-  const MAP_PINS = document.querySelector(`.map__pins`);
   const PIN_TEMPLATE = document.querySelector(`#pin`)
     .content
     .querySelector(`.map__pin`);
   const OFFSET_X = 25;
   const OFFSET_Y = 70;
 
-  window.renderPins = function (pins) {
-    const FRAGMENT = document.createDocumentFragment();
+  window.renderPin = function (pin) {
+    const PIN_ELEMENT = PIN_TEMPLATE.cloneNode(true);
 
-    for (let i = 0; i < pins.length; i++) {
-      const PIN_ELEMENT = PIN_TEMPLATE.cloneNode(true);
+    PIN_ELEMENT.style = `left: ${pin.location.x - OFFSET_X}px; top: ${pin.location.y - OFFSET_Y}px;`;
+    PIN_ELEMENT.querySelector(`img`).src = pin.author.avatar;
+    PIN_ELEMENT.querySelector(`img`).alt = pin.offer.title;
+    PIN_ELEMENT.addEventListener(`click`, function () {
+      window.showCard(pin);
+    });
 
-      PIN_ELEMENT.style = `left: ${pins[i].location.x - OFFSET_X}px; top: ${pins[i].location.y - OFFSET_Y}px;`;
-      PIN_ELEMENT.querySelector(`img`).src = pins[i].author.avatar;
-      PIN_ELEMENT.querySelector(`img`).alt = pins[i].offer.title;
-      FRAGMENT.appendChild(PIN_ELEMENT);
-      PIN_ELEMENT.addEventListener(`click`, function () {
-        window.showCard(pins[i]);
-      });
-    }
-    MAP_PINS.appendChild(FRAGMENT);
+    return PIN_ELEMENT;
   };
 })();
